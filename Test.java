625e9b67243d1f1742e7e25ba1e498596a2b6f26
Steve Davis <deevodavis71@gmail.com>
2017-09-17 18:50:03 +0100

    Changes

--- Test.java
+++ Test.java
@@ -6,7 +6,7 @@ public class SteveX2 {
 
 		System.out.println ("NEW More Hello to young Steves New MT2 Changed World!");
 
-		// More non conflixts
+		// More non conflixts 1111
 
 		// Do some work here! CONFLICT CHANGED HERE AS WELL
 		// New line that is changed
