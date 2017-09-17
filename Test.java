5e01370c92f10015db2f168b98b832cc657782ca
Steve Davis <deevodavis71@gmail.com>
2017-09-17 08:00:10 +0100

    Changes

--- Test.java
+++ Test.java
@@ -4,12 +4,13 @@ public class SteveX2 {
 
 	public static void main (String[] args) {
 
-		System.out.println ("Hello to Steves New MT2 Changed World!");
+		System.out.println ("Hello to young Steves New MT2 Changed World!");
 
 		// Do some work here!
 		// New line
 
 		// Some diff
+		// Another diff
 
 	}
 
